e076db6dd71e76bb742a36389b39f581dfbe2004
martinondiwa <surekondiwa@gmail.com>
2024-08-30 20:39:54 +0300

    exponential

--- Big_O_Notation/exponential.java
+++ Big_O_Notation/exponential.java
@@ -1,10 +1,19 @@
-public class exponential {
-    public class Innerexponential (n) {
-    count = 0 ;
-    for (var i = 1; i * i < n; i++){
-        //console.log(i);
-        count++;
+public class Exponential {
+    
+    // This method calculates how many integers squared are less than n
+    public int innerExponential(int n) {
+        int count = 0;
+        for (int i = 1; i * i < n; i++) {
+            // Uncomment the next line if you want to see the values of i
+            // System.out.println(i);
+            count++;
+        }
+        return count;
     }
-       return count 
+
+    public static void main(String[] args) {
+        Exponential exponentialCalculator = new Exponential();
+        int result = exponentialCalculator.innerExponential(16); // Example usage with n = 16
+        System.out.println("Count: " + result);
     }
 }
