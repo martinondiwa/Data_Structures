ea41f26cea68ca90ba5e7ea318cf4aa4ec2835c3
martinondiwa <surekondiwa@gmail.com>
2024-08-30 21:43:04 +0300

    big O

---
+++ Big_O_Notation/spacecomplexity.java
@@ -0,0 +1,11 @@
+package com.codewithmartin
+public interface spacecomplexity {
+    public void greet(String[] names) {
+        // 0(n) space
+        string[] copy = new String[names.length];
+        
+        for (int i=0; i < names.length; i++)
+            System.out.println("Hi" + names [i]);
+        }
+    }
+}
