c6b173b7a874a918f00cd16b64f3d44b4c9d809e
martinondiwa <surekondiwa@gmail.com>
2024-08-30 20:38:19 +0300

    exponential

---
+++ Big_O_Notation/exponential.java
@@ -0,0 +1,10 @@
+public class exponential {
+    public class Innerexponential (n) {
+    count = 0 ;
+    for (var i = 1; i * i < n; i++){
+        //console.log(i);
+        count++;
+    }
+       return count 
+    }
+}
