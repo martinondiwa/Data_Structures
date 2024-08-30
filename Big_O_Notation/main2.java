f61a449b4d759ce8e4dc40f79ebc94a152c0e900
martinondiwa <surekondiwa@gmail.com>
2024-08-30 19:44:35 +0300

    Big O

---
+++ Big_O_Notation/main2.java
@@ -0,0 +1,7 @@
+package com.codewithmartin
+public class main2 {
+    public void log (int[]numbers){
+        for (int i=0; i < numbers.length; i++)
+             System.out.print(numbers [i]);
+    }
+}
