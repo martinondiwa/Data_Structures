a1c5e312de9d694aef530259dc6441afbb083460
martinondiwa <surekondiwa@gmail.com>
2024-08-30 20:01:03 +0300

    Big O

---
+++ Big_O_Notation/Linearinceament.java
@@ -0,0 +1,10 @@
+package com.codewithmartin
+public class Linearinceament {
+    public void log(int[] numbers, string[] names) {
+        //0(n)
+        for (int number : numbers) // 0(n)
+            System.out.println(number);
+        for (string name : names) // 0(n)
+            System.out.println(name);
+    }
+}
