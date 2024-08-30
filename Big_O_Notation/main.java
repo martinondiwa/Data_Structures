374d94161c05e196cd17c22422fa14d80594acb9
martinondiwa <surekondiwa@gmail.com>
2024-08-30 19:37:17 +0300

    Big O

--- Big_O_Notation/main.java
+++ Big_O_Notation/main.java
@@ -2,6 +2,8 @@ package com.codewithmartin;
 
 public class main {
     public void log(int[] numbers){
+        //0(2) run time complexity of 2
+        system.out.println(numbers[0]);
         system.out.println(numbers[0]);
     }
 }
