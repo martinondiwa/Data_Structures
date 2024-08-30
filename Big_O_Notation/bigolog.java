08dca6bc644a558d6e4b28876b9ab9802e83339f
martinondiwa <surekondiwa@gmail.com>
2024-08-30 20:25:40 +0300

    log

--- Big_O_Notation/bigolog.java
+++ Big_O_Notation/bigolog.java
@@ -2,5 +2,8 @@ public class bigolog {
     // log curve slows at some point while the linear curve goes straight
     public class bigolog(n) {
         count = 0;
+        for (var i = 1; i < n; i = i*2){
+            
+        }
     }
 }
