7a7fc074dd5f8e810002935cec7d5479407507dc
martinondiwa <surekondiwa@gmail.com>
2024-08-30 20:29:07 +0300

    log

--- Big_O_Notation/bigolog.java
+++ Big_O_Notation/bigolog.java
@@ -3,7 +3,9 @@ public class bigolog {
     public class bigolog(n) {
         count = 0;
         for (var i = 1; i < n; i = i*2){
-            
+         
+            count ++;
         }
+        return count
     }
 }
